48f5957e4561f55f1260f1ccc0fb72bb7aa9c837
DCatcher <daniel.catchpole@live.com>
2013-08-02 13:47:27 +0100

    13:45 Commit

--- forge/mcp/src/minecraft/net/dcatcher/modjam/items/Disguiser.java
+++ forge/mcp/src/minecraft/net/dcatcher/modjam/items/Disguiser.java
@@ -5,6 +5,7 @@ import net.dcatcher.modjam.ModJam;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.EntityAIControlledByPlayer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
@@ -32,7 +33,7 @@ public class Disguiser extends Item {
 		EntityAIControlledByPlayer entityai = new EntityAIControlledByPlayer(eLiving, 1);
 		eLiving.tasks.addTask(1, new EntityAIControlledByPlayer(eLiving, 0.34F));
 
-		ReflectionHelper.findMethod(EntityLiving.class, EntityLiving.class, methodNames, methodTypes)
+		ReflectionHelper.findMethod(EntityLiving.class, EntityLivingBase.class, new String[] {"func_82171_bF", "");
 		
 		player.mountEntity(eLiving);
 	return true;
