af50e62ef52b216f059777ea2b7a8951f5207908
DCatcher <daniel.catchpole@live.com>
2013-08-02 12:58:52 +0100

    13:00 Commit

---
+++ forge/mcp/src/minecraft/net/dcatcher/modjam/ReflectedMethods.java
@@ -0,0 +1,11 @@
+package net.dcatcher.modjam;
+
+import net.minecraft.entity.passive.EntityCow;
+import cpw.mods.fml.relauncher.ReflectionHelper;
+
+public class ReflectedMethods {
+	
+	public static void reflect(){
+	}
+
+}
