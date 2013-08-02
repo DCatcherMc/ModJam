3a8369b4a774511c17ecd76858550245de8c3c13
DCatcher <daniel.catchpole@live.com>
2013-08-02 15:15:15 +0100

    15:15 Commit

--- forge/mcp/src/minecraft/net/dcatcher/modjam/ModJam.java
+++ forge/mcp/src/minecraft/net/dcatcher/modjam/ModJam.java
@@ -1,6 +1,7 @@
 package net.dcatcher.modjam;
 
 import net.dcatcher.modjam.blocks.BlockHandler;
+import net.dcatcher.modjam.entity.EntityReplacedSheep;
 import net.dcatcher.modjam.items.ItemHandler;
 import net.dcatcher.modjam.proxy.ClientProxy;
 import net.dcatcher.modjam.proxy.CommonProxy;
@@ -8,8 +9,12 @@ import net.dcatcher.modjam.tileentities.TileEntityInfernalBurner;
 import net.dcatcher.modjam.utils.DCConfig;
 import net.dcatcher.modjam.utils.DCCreativeTab;
 import net.dcatcher.modjam.utils.LanguageHandler;
+import net.minecraft.client.model.ModelSheep1;
+import net.minecraft.client.model.ModelSheep2;
+import net.minecraft.client.renderer.entity.RenderSheep;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraftforge.common.MinecraftForge;
+import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -18,6 +23,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 
@@ -50,6 +56,10 @@ public class ModJam {
 		GameRegistry.registerTileEntity(TileEntityInfernalBurner.class, "tileEntityInfernalBurner");
 	
 		MinecraftForge.EVENT_BUS.register(net.dcatcher.modjam.utils.EventHandler.class);
+		
+		
+		EntityRegistry.registerModEntity(EntityReplacedSheep.class, "ReplacedSheep", 1, this, 80, 3, true);
+		RenderingRegistry.registerEntityRenderingHandler(EntityReplacedSheep.class, new RenderSheep(new ModelSheep2(), new ModelSheep1(), 0.7F));
 	
 	}
 	
