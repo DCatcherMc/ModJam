4c78c73c7db8ee70d49c45ff1d52505018befc32
DCatcher <daniel.catchpole@live.com>
2013-08-02 13:15:55 +0100

    13:15 Commit

---
+++ forge/mcp/src/minecraft/net/dcatcher/modjam/utils/EventHandler.java
@@ -0,0 +1,23 @@
+package net.dcatcher.modjam.utils;
+
+import net.dcatcher.modjam.items.ItemHandler;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.ai.EntityAIControlledByPlayer;
+import net.minecraftforge.event.ForgeSubscribe;
+import net.minecraftforge.event.entity.EntityEvent;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.event.entity.player.EntityInteractEvent;
+
+public class EventHandler {
+
+	@ForgeSubscribe
+	public void entityInteractEvent(EntityInteractEvent e){
+		EntityLiving entity = (EntityLiving)e.target;
+		
+		if(e.entityPlayer.getCurrentEquippedItem().itemID == ItemHandler.disguiser.itemID){
+		e.entityPlayer.mountEntity(e.target);
+		entity.tasks.addTask(1, new EntityAIControlledByPlayer(entity, 0.34F));
+		}
+	}
+}
