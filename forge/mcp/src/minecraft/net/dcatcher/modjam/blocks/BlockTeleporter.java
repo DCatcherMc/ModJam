53803e70cf738150a58cff66cf7ec957b7046692
DCatcher <daniel.catchpole@live.com>
2013-08-02 11:45:25 +0100

    11:35 Commit

---
+++ forge/mcp/src/minecraft/net/dcatcher/modjam/blocks/BlockTeleporter.java
@@ -0,0 +1,27 @@
+package net.dcatcher.modjam.blocks;
+
+import net.minecraft.block.BlockContainer;
+import net.minecraft.block.material.Material;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
+public class BlockTeleporter extends BlockContainer {
+
+	public BlockTeleporter(int par1, Material par2Material) {
+		super(par1, par2Material);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public boolean canConnectRedstone(IBlockAccess world, int x, int y, int z,
+			int side) {
+		return true;
+	}
+	
+	@Override
+	public TileEntity createNewTileEntity(World world) {
+		return null;
+	}
+
+}
