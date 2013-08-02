af1385383f5e928edcf263df19a4592eba4fa955
DCatcher <daniel.catchpole@live.com>
2013-08-02 15:15:32 +0100

    15:15 Commit 2

---
+++ forge/mcp/src/minecraft/net/dcatcher/modjam/entity/ContainerReplacedSheep.java
@@ -0,0 +1,20 @@
+package net.dcatcher.modjam.entity;
+
+import net.minecraft.entity.passive.EntitySheep;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
+
+class ContainerSheep extends Container
+{
+    final EntityReplacedSheep field_90034_a;
+
+    ContainerSheep(EntityReplacedSheep par1EntitySheep)
+    {
+        this.field_90034_a = par1EntitySheep;
+    }
+
+    public boolean canInteractWith(EntityPlayer par1EntityPlayer)
+    {
+        return false;
+    }
+}
