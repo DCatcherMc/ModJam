c25aaa1fea1ce3911f1bd7e30d74f74180a65b3a
DCatcher <daniel.catchpole@live.com>
2013-08-03 12:16:54 +0100

    12:15 Commit: Taking a lunch break :)

--- forge/mcp/src/minecraft/net/dcatcher/modjam/items/Disguiser.java
+++ forge/mcp/src/minecraft/net/dcatcher/modjam/items/Disguiser.java
@@ -3,6 +3,9 @@ package net.dcatcher.modjam.items;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.dcatcher.modjam.ModJam;
+import net.dcatcher.modjam.entity.cow.EntityReplacedCow;
+import net.dcatcher.modjam.entity.hostile.enderman.EntityReplacedEnderman;
+import net.dcatcher.modjam.entity.hostile.spider.EntityReplacedSpider;
 import net.dcatcher.modjam.entity.sheep.EntityReplacedSheep;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
@@ -11,6 +14,9 @@ import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.EntityAIControlledByPlayer;
 import net.minecraft.entity.item.EntityFallingSand;
+import net.minecraft.entity.monster.EntityEnderman;
+import net.minecraft.entity.monster.EntitySpider;
+import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.entity.passive.EntitySheep;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
@@ -37,29 +43,68 @@ public class Disguiser extends Item {
 		if(entity instanceof EntityFallingSand){
 			return false;
 		}
-		if(entity instanceof EntitySheep ){
 			double xCoord = entity.posX;
 			double yCoord = entity.posY;
 			double zCoord = entity.posZ;
 			float yaw = entity.rotationYaw;
 			float pitch = entity.rotationPitch;
 			
-			
+
 			World world = entity.worldObj;
 			entity.setDead();
-			Entity e = new EntityReplacedSheep(world);
-			e.setLocationAndAngles(xCoord, yCoord, zCoord, yaw, pitch);
-			if(!world.isRemote){
-				world.spawnEntityInWorld(e);
-			}
+			if(entity instanceof EntitySheep ){
+				Entity e = new EntityReplacedSheep(world);
+				e.setLocationAndAngles(xCoord, yCoord, zCoord, yaw, pitch);
+				if(!world.isRemote){
+					world.spawnEntityInWorld(e);
+				}
+				
+				
+				EntityLiving replacedMob = (EntityLiving)e;
+				
+				replacedMob.tasks.addTask(1, new EntityAIControlledByPlayer(replacedMob, 1F));
+				}
+
 			
-			EntityLiving replacedMob = (EntityLiving)e;
-			replacedMob.tasks.addTask(1, new EntityAIControlledByPlayer(replacedMob, 1F));
-			world.updateEntities();
+			if(entity instanceof EntityCow ){
+				Entity e = new EntityReplacedCow(world);
+				e.setLocationAndAngles(xCoord, yCoord, zCoord, yaw, pitch);
+				if(!world.isRemote){
+					world.spawnEntityInWorld(e);
+				}
+				
+				
+				EntityLiving replacedMob = (EntityLiving)e;
+				
+				replacedMob.tasks.addTask(1, new EntityAIControlledByPlayer(replacedMob, 1F));
+				}
+			
+			if(entity instanceof EntityEnderman ){
+				Entity e = new EntityReplacedEnderman(world);
+				e.setLocationAndAngles(xCoord, yCoord, zCoord, yaw, pitch);
+				if(!world.isRemote){
+					world.spawnEntityInWorld(e);
+				}
+				
+				
+				EntityLiving replacedMob = (EntityLiving)e;
+				
+				replacedMob.tasks.addTask(1, new EntityAIControlledByPlayer(replacedMob, 1F));
+				}
 		
-		}
-		
-		
+			
+			if(entity instanceof EntitySpider ){
+				Entity e = new EntityReplacedSpider(world);
+				e.setLocationAndAngles(xCoord, yCoord, zCoord, yaw, pitch);
+				if(!world.isRemote){
+					world.spawnEntityInWorld(e);
+				}
+				
+				
+				EntityLiving replacedMob = (EntityLiving)e;
+				
+				replacedMob.tasks.addTask(1, new EntityAIControlledByPlayer(replacedMob, 1F));
+				}
 	return true;
 	}
 	
