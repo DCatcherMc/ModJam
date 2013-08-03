895695729c6ba683b0f33c646fe1f7607e685e54
DCatcher <daniel.catchpole@live.com>
2013-08-03 15:30:25 +0100

    3:30PM Commit (Day 2)

--- forge/mcp/src/minecraft/net/dcatcher/modjam/items/ItemHandler.java
+++ forge/mcp/src/minecraft/net/dcatcher/modjam/items/ItemHandler.java
@@ -7,18 +7,14 @@ public class ItemHandler {
 
 	public static Item diamondShard, ballOfCrushedDiamond;
 	
-	public static Item teleporterShard;
-	public static Item disguiseTag;
 	public static Item disguiser;
 	
 	public static void registerItems(){
 		diamondShard = new ItemDiamondShard(DCConfig.idDiamondShard -256);
 		ballOfCrushedDiamond = new ItemBallCrushedDiamond(DCConfig.idCrushedDiamond -256);
 		
-		teleporterShard = new ItemTeleporterShard(DCConfig.idTeleporterShard -256);
 		
-		disguiseTag = new ItemDisguiseTag(DCConfig.idDisguiserTag);
-		disguiser = new Disguiser(DCConfig.idDisguiser);
+		disguiser = new Disguiser(DCConfig.idDisguiser -256);
 		
 	}
 
